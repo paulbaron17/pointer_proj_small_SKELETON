3897d59238b9fa4790a46286dfd72db618b80661
Guest <paulbaron17>
2021-03-20 19:06:48 -0400

    done

--- src/pointer_proj_small.cpp
+++ src/pointer_proj_small.cpp
@@ -17,7 +17,6 @@ using namespace KP;
 
 int main(int argc, char *argv[]) {
 
-
 	if (*argv[0] == HELP_CHAR) {
 		cout << HELP_STRING1 << endl;
 		cout << HELP_STRING2 << endl;
@@ -38,25 +37,25 @@ int main(int argc, char *argv[]) {
 
 		const char *src = string1.c_str();
 		const char *tmp_tag = tag.c_str();
-		int numOfTags = findNumbOccurrences(src,tmp_tag);
+		int numOfTags = findNumbOccurrences(src, tmp_tag);
 
 		int len_str = strlen(src);
 		int len_tag = strlen(tmp_tag);
 		const char *tmp_tr = tag_replacement.c_str();
 		int len_tr = strlen(tmp_tr);
-		int memory = amountOfMemoryToAllocateForNewString(len_str, numOfTags, len_tag, len_tr);
+		int memory = amountOfMemoryToAllocateForNewString(len_str, numOfTags,
+				len_tag, len_tr);
 
 		char *string2 = new char[memory];
 		memset(string2, 0, memory * sizeof(char));
 
-
-		replace(src, string2, tmp_tag,tmp_tr);
+		replace(src, string2, tmp_tag, tmp_tr);
 
 		string filename;
 		writeFile(outp, string2);
 
-		if(string2){
-			delete [] string2;
+		if (string2) {
+			delete[] string2;
 			string2 = 0;
 		}
 
